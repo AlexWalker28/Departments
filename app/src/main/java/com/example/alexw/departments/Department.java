dc2bb3c4179f4fcafde945c3b358963a89b5b35c
AlexWalker <alexwolk28@gmail.com>
2017-02-17 15:49:41 +0600

    Format Department class

--- app/src/main/java/com/example/alexw/departments/Department.java
+++ app/src/main/java/com/example/alexw/departments/Department.java
@@ -2,16 +2,12 @@ package com.example.alexw.departments;
 
 import com.google.android.gms.maps.model.LatLng;
 
-/**
- * This class is special feature too.
- */
-public class Department {
 
-    /*public Department(String name, String latitude, String longitude) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }*/
+public class Department {
+    public Department() {}
+
+    private String name;
+    private LatLng latLng;
 
     public Department(String name, LatLng latLng){
         this.name = name;
@@ -25,17 +21,5 @@ public class Department {
     public LatLng getLatLng(){
         return this.latLng;
     }
-    public String getLatitude(){
-        return this.latitude;
-    }
-    public String getLongitude(){
-        return this.longitude;
-    }
-
-    private String name;
-    private LatLng latLng;
-    private String latitude;
-    private String longitude;
-
 
 }
