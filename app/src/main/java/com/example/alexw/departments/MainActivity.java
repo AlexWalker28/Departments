59509c5f00b90b139910dc4daefe7c526a78ef3b
AlexWalker28 <alexwolk28@gmail.com>
2017-08-26 21:43:28 +0600

    Fix map type switch bug

--- app/src/main/java/com/example/alexw/departments/MainActivity.java
+++ app/src/main/java/com/example/alexw/departments/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -71,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         switchCompat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.isSelected()){
+                if(v.isActivated()){
                     MapsFragment.getMap().setMapType(GoogleMap.MAP_TYPE_NORMAL);
                     v.setActivated(false);
                 } else {
