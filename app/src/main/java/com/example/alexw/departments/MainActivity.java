5048eb27c98730f96c0f3e6f2a9aefb5eb0a4478
AlexWalker28 <alexwolk28@gmail.com>
2017-08-26 23:25:37 +0600

    Add sign in option

--- app/src/main/java/com/example/alexw/departments/MainActivity.java
+++ app/src/main/java/com/example/alexw/departments/MainActivity.java
@@ -18,24 +18,33 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 10;
     private MenuItem reportMistakeMenuItem;
     private MenuItem goKGMAMenuItem;
     private DrawerLayout drawerLayout;
-    private ArrayList<String> drawerDataArrayList;
     private ActionBarDrawerToggle drawerToggle;
     private Toolbar toolbar;
     private NavigationView navigationView;
     private ViewPager viewPager;
     private String [] tabTitlesArray;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private MenuItem accountMenuItem;
 
 
     @Override
@@ -44,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tabTitlesArray = getResources().getStringArray(R.array.tab_titles);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         viewPager = (ViewPager)findViewById(R.id.viewpager);
         CustomFragmentPagerAdapter adapter = new CustomFragmentPagerAdapter(getSupportFragmentManager(), tabTitlesArray);
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        accountMenuItem = navigationView.getMenu().findItem(R.id.account_item);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -62,7 +73,22 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(drawerToggle);
 
         handleMapTypeSwitch();
+        getCurrentUser();
 
+    }
+
+    private void getCurrentUser() {
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    accountMenuItem.setTitle(R.string.sign_out);
+                } else {
+                    accountMenuItem.setTitle(R.string.sign_in);
+                }
+            }
+        };
     }
 
     private void handleMapTypeSwitch() {
@@ -114,15 +140,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void selectDrawerItem(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.nav_first_fragment:
+            case R.id.first_tab_item:
                 viewPager.setCurrentItem(0, true);
                 break;
-            case R.id.nav_second_fragment:
+            case R.id.second_tab_item:
                 viewPager.setCurrentItem(1, true);
                 break;
-
+            case R.id.account_item:
+                if(user == null){
+                    signIn();
+                } else {
+                    firebaseAuth.signOut();
+                }
         }
         drawerLayout.closeDrawers();
+    }
+
+    private void signIn() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(),
+                RC_SIGN_IN);
     }
 
 
@@ -160,6 +203,20 @@ public class MainActivity extends AppCompatActivity {
         Intent goKGMA = new Intent(Intent.ACTION_VIEW);
         goKGMA.setData(Uri.parse("https://vk.com/lovekgma"));
         startActivity(goKGMA);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 
 }
